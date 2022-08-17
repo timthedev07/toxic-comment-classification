75d31f76da67d462666888093602c501f56fe902
Tim <harvard90873@gmail.com>
2022-08-17 23:53:21 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
+import tensorflow as tf
+from flask import Flask, render_template, request
+from nltk.corpus import stopwords
+import os
+stop_words = set(stopwords.words('english'))
 
+SAVED_MODEL_DIR = "model"
+MODEL_VERSION = "v1"
+
+app = Flask(__name__)
+
+
+@tf.function
 def custom_standardization(text: tf.Tensor):
     # to lower case
     text = tf.strings.lower(text)
@@ -32,3 +44,36 @@ def custom_standardization(text: tf.Tensor):
             text, f"[^A-Za-z0-9_]+{i}[^A-Za-z0-9_]+", " ")
 
     return text
+
+
+def loadModel():
+    custom_objects = {"custom_standardization": custom_standardization}
+    with tf.keras.utils.custom_object_scope(custom_objects):
+        loaded = tf.keras.models.load_model(
+            os.path.join(SAVED_MODEL_DIR, MODEL_VERSION))
+        return loaded
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        data = request.get_json()
+
+        if not data["text"]:
+            return "Bad Request", 400
+
+        model = loadModel()
+        [res] = model([data["text"]])
+
+        print(res)
+
+        return {
+            "value": str(res),
+            "sentiment": "negative" if res < 0 else ("positive" if res > 0 else "neutral")
+        }, 200
+    else:
+        return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
